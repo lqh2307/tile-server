d73797d5879136a4a4cf17603a3727cf933efaf3
lqh2307 <huylq23072000@gmail.com>
2024-08-25 04:43:09 +0000

    1.1.0

---
+++ src/process_image_worker.js
@@ -0,0 +1,16 @@
+import { parentPort, workerData } from "node:worker_threads";
+import { processImage } from "./utils.js";
+
+(async () => {
+  try {
+    const data = await processImage(workerData.data, workerData.scale, workerData.compression, workerData.tileSize, workerData.z);
+
+    parentPort.postMessage({
+      data: data,
+    });
+  } catch (error) {
+    parentPort.postMessage({
+      error: error.message,
+    });
+  }
+})();
