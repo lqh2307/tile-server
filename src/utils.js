caacfcd87b73acd6882a255073da2fec5f8fa544
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-10-30 10:28:54 +0700

    1.0.0

--- src/utils.js
+++ src/utils.js
@@ -409,107 +409,47 @@ export async function seedXYZTileDataFiles(
 }
 
 /**
- * Download all xyz tile data files in a specified bounding box and zoom levels
- * @param {string} tileURL Tile URL to download
+ * Remove all xyz tile data files in a specified zoom levels
  * @param {string} outputFolder Folder to store downloaded tiles
- * @param {"jpeg"|"jpg"|"pbf"|"png"|"webp"|"gif"} format Tile format
- * @param {Array<number>} bbox Bounding box in format [lonMin, latMin, lonMax, latMax] in EPSG:4326
- * @param {number} minZoom Minimum zoom level
- * @param {number} maxZoom Maximum zoom level
- * @param {"xyz"|"tms"} scheme Tile scheme
- * @param {number} concurrency Concurrency download
- * @param {boolean} overwrite Overwrite exist file
- * @param {number} maxTry Number of retry attempts on failure
- * @param {number} timeout Timeout in milliseconds
+ * @param {Array<number>} zoomLevels Zoom levels
  * @returns {Promise<void>}
  */
-export async function seedXYZTileDataFiles(
-  tileURL,
+export async function removeXYZTileDataFiles(
   outputFolder,
-  format,
-  bbox = [-180, -85.051129, 180, 85.051129],
-  minZoom = 0,
-  maxZoom = 22,
-  scheme = "xyz",
-  concurrency = os.cpus().length,
-  overwrite = true,
-  maxTry = 5,
-  timeout = 60000
+  zoomLevels = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22,
+  ]
 ) {
-  const tiles = getTilesFromBBox(bbox, minZoom, maxZoom, scheme);
-  const limitConcurrencyDownload = pLimit(concurrency);
   let hashs = {};
 
   try {
     hashs = JSON.parse(await fsPromise.readFile(`${outputFolder}/md5.json`));
   } catch (error) {}
 
-  printLog(
-    "info",
-    `Downloading ${tiles.length} tile data files - BBox [${bbox.join(
-      ", "
-    )}] - Zoom level ${minZoom} -> ${maxZoom}...`
-  );
-
   await Promise.all(
-    tiles.map((tile) =>
-      limitConcurrencyDownload(async () => {
-        const url = tileURL.replace(
-          "/{z}/{x}/{y}",
-          `/${tile[0]}/${tile[1]}/${tile[2]}`
-        );
+    zoomLevels.map(async (zoomLevel) => {
+      const files = await findFiles(
+        `${outputFolder}/${zoomLevel}`,
+        /^\d+\.(jpeg|jpg|pbf|png|webp|gif)$/,
+        true
+      );
 
-        const filePath = `${outputFolder}/${tile[0]}/${tile[1]}/${tile[2]}.${format}`;
+      files.forEach((file) => {
+        delete (hashs, file.slice(0, file.lastIndexOf(".")));
+      });
 
-        try {
-          if (overwrite === false && (await isExistFile(filePath)) === true) {
-            printLog(
-              "info",
-              `Tile data file is exist. Skipping download tile data file from ${url}...`
-            );
-          } else {
-            printLog("info", `Downloading tile data file from ${url}...`);
+      await fsPromise.writeFile(
+        `${outputFolder}/md5.json`,
+        JSON.stringify(hashs, null, 2)
+      );
 
-            await retry(async () => {
-              // Get data
-              const response = await getData(url, timeout);
-
-              // Store data to file
-              await fsPromise.mkdir(path.dirname(filePath), {
-                recursive: true,
-              });
-
-              await fsPromise.writeFile(filePath, response.data);
-
-              // Store data md5 hash
-              if (response.headers["Etag"]) {
-                hashs[`${tile[0]}/${tile[1]}/${tile[2]}`] =
-                  response.headers["Etag"];
-              } else {
-                hashs[`${tile[0]}/${tile[1]}/${tile[2]}`] = calculateMD5(
-                  response.data
-                );
-              }
-            }, maxTry);
-          }
-        } catch (error) {
-          printLog("error", `Failed to download tile data file: ${error}`);
-
-          // Remove error tile data file
-          await fsPromise.rm(filePath, {
-            force: true,
-          });
-        }
-      })
-    )
+      await fsPromise.rm(`${outputFolder}/${zoomLevel}`, {
+        force: true,
+        recursive: true,
+      });
+    })
   );
-
-  await fsPromise.writeFile(
-    `${outputFolder}/md5.json`,
-    JSON.stringify(hashs, null, 2)
-  );
-
-  await removeEmptyFolders(outputFolder);
 }
 
 /**
