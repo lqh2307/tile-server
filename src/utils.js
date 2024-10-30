10e236604acc83b9fc99f00b82b7ac951b3e00e5
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-10-30 10:35:15 +0700

    1.0.0

--- src/utils.js
+++ src/utils.js
@@ -438,7 +438,7 @@ export async function removeXYZTileDataFiles(
       );
 
       files.forEach((file) => {
-        delete hashs[path.basename(file, `.${format}`)]
+        delete hashs[path.basename(file, `.${format}`)];
       });
 
       await fsPromise.writeFile(
