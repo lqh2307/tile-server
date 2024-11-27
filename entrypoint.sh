87f6cb45496fa2da81a53a6df4ad708eb722b9ed
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-11-27 17:40:12 +0700

    1.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 while true; do
-  if [ -z "$DISPLAY" ]; then
+  if [ -z "$DISPLAY" ] && [ $XVFB_STARTED -eq 0 ]; then
     xvfb-run -a -s "-terminate -nolisten unix" node ./src/main.js "$@"
+
+    XVFB_STARTED=1
   else
     node ./src/main.js "$@"
   fi
@@ -11,11 +13,9 @@ while true; do
 
   if [ $EXIT_CODE -eq 0 ]; then
     break
-  else
-    if [ $EXIT_CODE -ne 1 ]; then
-      echo "Server exited with code: $EXIT_CODE. Restarting server after 5 seconds..."
+  elif [ $EXIT_CODE -ne 1 ]; then
+    echo "Server exited with code: $EXIT_CODE. Restarting server after 5 seconds..."
 
-      sleep 5
-    fi
+    sleep 5
   fi
 done
