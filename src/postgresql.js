4bf5f185fe4cb9e6182de50d82679040d89753e4
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-12-25 23:29:02 +0700

    1.0.0

--- src/postgresql.js
+++ src/postgresql.js
@@ -21,12 +21,11 @@ export async function openPostgreSQL(uri, isCreate) {
       await tmpClient.connect();
 
       const res = await tmpClient.query(
-        "SELECT FROM pg_database WHERE datname = $1;",
-        [dbName]
+        `SELECT FROM pg_database WHERE datname = "${dbName}";`
       );
 
       if (res.rows.length === 0) {
-        await tmpClient.query("CREATE DATABASE $1;", [dbName]);
+        await tmpClient.query(`CREATE DATABASE "${dbName}";`);
       }
     } catch (error) {
       if (tmpClient !== undefined) {
